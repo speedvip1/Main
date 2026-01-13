52577ece4d17b41f3f37d05376045778cb9e478c
speedvip1 <1speedhubx@gmail.com>
2026-01-13 19:57:45 +0300

    Create Themes.lua

---
+++ Themes.lua
@@ -0,0 +1,20 @@
+local Themes = {
+  ["Default"] = {
+    ["Color Hub 1"] = Color3.fromRGB(18, 18, 18),
+    ["Color Hub 2"] = Color3.fromRGB(25, 25, 25),
+    ["Color Theme"] = Color3.fromRGB(30, 150, 240),
+    ["Color Text"] = Color3.fromRGB(240, 240, 240),
+    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
+    ["Color Stroke"] = Color3.fromRGB(80, 80, 80),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Quad
+  },
+}
+
+return Themes
